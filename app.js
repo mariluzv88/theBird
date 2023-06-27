a4cc780c2b71e5683b1e1fa648120e420502cb76
Mariluz Vargas <mariluzv88@gmail.com>
2023-06-27 15:47:28 -0400

    started Wire framing

---
+++ app.js
@@ -0,0 +1,14 @@
+// dom eventlistener load html first
+    // grab bird as function
+    // grab game Board / window as function
+    // grab floor as function
+
+    // move bird to center of skyContainer
+    // variable of bird margin =px
+    // variable of bird end = px
+    // variable of bird weight
+
+    // game loop
+        // call bird weight asign and minus gracity
+        // call bird end asign it bird end variable 
+        // call bird and style it margin /asgin bird margin variable
